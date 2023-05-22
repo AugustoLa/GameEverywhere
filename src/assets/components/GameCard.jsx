e30715f0aa68b92820cb3439cf0bae1002b8d147
AugustoLae <augusto_florindo@hotmail.com>
2023-05-22 17:29:03 -0300

    cards adicionados a home page

--- src/assets/components/GameCard.jsx
+++ src/assets/components/GameCard.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import { FaStar } from 'react-icons/fa'
+import '../pages/GameGrid.css'
 
-function GameCard() {
+function GameCard({ game, showLink = true }) {
     return (
-        <div>GameCard</div>
+        <div className="movie-card">
+            <img src={game.background_image} alt={game.name} />
+            <h2>{game.name}</h2>
+            <p>
+                <FaStar /> {game.metacritic}
+            </p>
+            {showLink && <Link to={`/game/${game.id}`}>Detalhes</Link>}
+        </div>
     )
 }
 
