b0feb9d14e6f420f80aaff3522f001729830db2f
AugustoLae <augusto_florindo@hotmail.com>
2023-05-22 17:24:45 -0300

    estruturação de rotas e pastas adicionadas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './assets/pages/Home.jsx'
 import App from './App.jsx'
+import Game from './assets/pages/Game.jsx'
+import Search from './assets/pages/Search.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App></App>}>
+          <Route path='/' element={<Home />} />
+          <Route path='game/:id' element={<Game />} />
+          <Route path='search' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
