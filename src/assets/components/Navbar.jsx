dfe2694b0965d7d9e1b006b933feb8a5d6d5f6da
AugustoLae <augusto_florindo@hotmail.com>
2023-05-22 17:27:36 -0300

    componente navbar adicionado, estilização

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import './Navbar.css'
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <nav id='navbar'>
+            <h2>
+                <Link to='/'><BiCameraMovie />GamesLib</Link>
+            </h2>
+            <form>
+                <input type='text' placeholder='Busque um game'></input>
+                <button type='submit'>
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+        </nav>
     )
 }
 
